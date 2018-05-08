c4c16654ad0dfdb7b323e2df26fcb8bfee308ed2
bhauth <b@name.com>
2018-05-07 20:50:17 -0600

    venom mage rework: - Ignite Poison replaces Meph in starter book - start with blowgun and 2 poison needles - Ignite becomes level 2, Vapours becomes level 3 - add Poison Arrow to another spellbook - adjustments to Ignite and Vapours scaling

--- crawl-ref/source/job-data.h
+++ crawl-ref/source/job-data.h
@@ -213,19 +213,8 @@ static const map<job_type, job_def> job_data =
     { { SK_FIGHTING, 2 }, { SK_ARMOUR, 1 }, { SK_DODGING, 2 },
       { SK_SPELLCASTING, 2 }, { SK_CHARMS, 3 }, { SK_WEAPON, 2 }, },
 } },
- 
- { JOB_SLIME_KNIGHT, {
-    "Sl", "Slime Knight",
-    4, 4, 4,
-    { SP_HILL_ORC, SP_TROLL, SP_CENTAUR, SP_MERFOLK, SP_MINOTAUR,
-      SP_BASE_DRACONIAN, SP_DEMONSPAWN, },
-    { "leather armour plus:2" },
-    WCHOICE_PLAIN,
-    { { SK_FIGHTING, 3 }, { SK_ARMOUR, 1 }, { SK_DODGING, 1 },
-      { SK_WEAPON, 3 } },
-} },   
-    
- { JOB_SUMMONER, {
+
+{ JOB_SUMMONER, {
     "Su", "Summoner",
     0, 7, 5,
     { SP_DEEP_ELF, SP_HILL_ORC, SP_VINE_STALKER, SP_MERFOLK, SP_TENGU,
@@ -252,7 +241,7 @@ static const map<job_type, job_def> job_data =
     0, 7, 5,
     { SP_DEEP_ELF, SP_SPRIGGAN, SP_NAGA, SP_MERFOLK, SP_TENGU, SP_FELID,
       SP_DEMONSPAWN, },
-    { "robe", "Young Poisoner's Handbook" },
+    { "robe", "Young Poisoner's Handbook", "blowgun", "needle ego:poisoned q:2"},
     WCHOICE_NONE,
     { { SK_CONJURATIONS, 1 }, { SK_POISON_MAGIC, 3 }, { SK_SPELLCASTING, 2 },
       { SK_DODGING, 2 }, { SK_STEALTH, 2 }, },
