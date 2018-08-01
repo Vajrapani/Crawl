0658f7d3e1100a5e14bdfda65cc51e7ad218d8b8
bhauth <b@name.com>
2018-08-01 07:25:44 -0600

    fix pickup msg

--- crawl-ref/source/items.cc
+++ crawl-ref/source/items.cc
@@ -1701,20 +1701,7 @@ int find_free_slot(const item_def &i)
 }
 
 static void _got_item(item_def& item)
-{
-    seen_item(item);
-    
-    int nrunes = runes_in_pack();
-    if (nrunes >= 1 && !fully_identified(item))
-    {
-        mpr("You use a rune to identify the item.");
-        if (item.defined())
-        {
-            set_ident_type(item, true);
-            set_ident_flags(item, ISFLAG_IDENT_MASK);
-        }
-    }
-    
+{    
     shopping_list.cull_identical_items(item);
     item.flags |= ISFLAG_HANDLED;
 
@@ -1766,7 +1753,10 @@ static bool _put_item_in_inv(item_def& it, int quant_got, bool quiet, bool& put_
     // sanity
     if (quant_got > it.quantity || quant_got <= 0)
         quant_got = it.quantity;
-
+    
+    // moved this here from later; used to happen on successful pickup only
+    seen_item(it);
+    
     // attempt to put the item into your inventory.
     int inv_slot;
     if (_merge_items_into_inv(it, quant_got, inv_slot, quiet))
@@ -2231,6 +2221,17 @@ static bool _merge_items_into_inv(item_def &it, int quant_got,
     {
         quant_got = 1;
         return true;
+    }
+        
+    int nrunes = runes_in_pack();
+    if (nrunes >= 1 && !fully_identified(it))
+    {
+        mpr("You use a rune to identify the item.");
+        if (it.defined())
+        {
+            set_ident_type(it, true);
+            set_ident_flags(it, ISFLAG_IDENT_MASK);
+        }
     }
 
     // Can't combine, check for slot space.
